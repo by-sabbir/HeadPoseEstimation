369704b72194354fed7b7179270483c2150c1abd
Shubham Shah <shubham.shah09@gmail.com>
2021-04-08 08:31:59 +0530

    Added Faster and accurate face detection pipeline

--- head_pose_from_webcam.py
+++ head_pose_from_webcam.py
@@ -6,12 +6,18 @@ import dlib
 import argparse
 import numpy as np
 
-#import Face Recognition
-import face_recognition
+#import Face Recognition libraries
+import mediapipe as mp
 
 # helper modules
 from drawFace import draw
 import reference_world as world
+
+#Settingup MediaPipe
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection(
+    min_detection_confidence=0.5)
 
 PREDICTOR_PATH = os.path.join("models", "shape_predictor_68_face_landmarks.dat")
 
@@ -34,33 +40,44 @@ def main():
     predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
     cap = cv2.VideoCapture(args["camsource"])
+    
 
     while True:
         GAZE = "Face Not Found"
         ret, img = cap.read()
+        image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        h, w, c = image.shape
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = face_detection.process(image)
+
+        # Draw the face detection annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if not ret:
             print(f'[ERROR - System]Cannot read from source: {args["camsource"]}')
             break
 
-        #faces = detector(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
-        faces = face_recognition.face_locations(img, model="cnn")
+        if results.detections:
+            for detection in results.detections:
+                location = detection.location_data
+                relative_bounding_box = location.relative_bounding_box
+                x_min = relative_bounding_box.xmin
+                y_min = relative_bounding_box.ymin
+                widthh = relative_bounding_box.width
+                heightt = relative_bounding_box.height
 
-        for face in faces:
 
-            #Extracting the co cordinates to convert them into dlib rectangle object
-            x = int(face[3])
-            y = int(face[0])
-            w = int(abs(face[1]-x))
-            h = int(abs(face[2]-y))
-            u=int(face[1])
-            v=int(face[2])
-
-            newrect = dlib.rectangle(x,y,u,v)
-            cv2.rectangle(img, (x, y), (x+w, y+h),
+                absx,absy=mp_drawing._normalized_to_pixel_coordinates(x_min,y_min,w,h)
+                abswidth,absheight = mp_drawing._normalized_to_pixel_coordinates(x_min+widthh,y_min+heightt,w,h)
+                
+            newrect = dlib.rectangle(absx,absy,abswidth,absheight)
+            cv2.rectangle(image, (absx, absy), (abswidth, absheight),
             (0, 255, 0), 2)
             shape = predictor(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), newrect)
 
-            draw(img, shape)
+            draw(image, shape)
 
             refImgPts = world.ref2dImagePoints(shape)
 
@@ -81,7 +98,7 @@ def main():
             #  draw nose line
             p1 = (int(refImgPts[0, 0]), int(refImgPts[0, 1]))
             p2 = (int(noseEndPoint2D[0, 0, 0]), int(noseEndPoint2D[0, 0, 1]))
-            cv2.line(img, p1, p2, (110, 220, 0),
+            cv2.line(image, p1, p2, (110, 220, 0),
                      thickness=2, lineType=cv2.LINE_AA)
 
             # calculating euler angles
@@ -103,8 +120,8 @@ def main():
             else:
                 GAZE = "Forward"
 
-        cv2.putText(img, GAZE, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 80), 2)
-        cv2.imshow("Head Pose", img)
+        cv2.putText(image, GAZE, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 80), 2)
+        cv2.imshow("Head Pose", image)
 
         key = cv2.waitKey(10) & 0xFF
         if key == 27:
